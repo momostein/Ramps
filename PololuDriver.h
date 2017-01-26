f987f418da770db683bf49bf3745209d55fb6399
momostein <brecht.ooms@hotmail.com>
2017-01-26 12:44:58 +0100

    NOT DONE YET
    adding manual stepping mode and changing previous to autoStep

--- PololuDriver.h
+++ PololuDriver.h
@@ -17,14 +17,19 @@ class pololuStepper
 {
 	public:
 		pololuStepper(int _stepPin, int _dirPin, int _enablePin);
-		
-		void step(long amount); //Beweeg een aantal stappen in de gekozen richting (positief of negatief)
+
+    void setDir(short _dir);
+    void stepOn();
+    void stepOff();
+    
+		void autoStep(long amount); //Beweeg een aantal stappen in de gekozen richting (positief of negatief)
 		void moveTo(long targetPos); //Beweeg naar doelpositie
 		
 		long position = 0; //Positie
 		unsigned int period = 1000; //Periode per stem in microseconden
 
 	private:
+    int dir = 1;
 		int stepPin;
 		int dirPin;
 		int enablePin;
