7b2cf77906e634c955ea3f856132ba7d172074f0
Momostein - Brecht Ooms <brecht.ooms@hotmail.com>
2017-01-31 17:26:01 +0100

    Updated EndstopTest.ino
    The EndstopTest example now checks every endstop

--- examples/EndstopTest/EndstopTest.ino
+++ examples/EndstopTest/EndstopTest.ino
@@ -11,6 +11,15 @@ void setup()
 
 void loop()
 {
-    Serial.println(digitalRead(X_MIN_PIN));
+    Serial.print("Min X Y Z: ")
+    Serial.print(digitalRead(X_MIN_PIN));
+    Serial.print(digitalRead(Y_MIN_PIN));
+    Serial.print(digitalRead(Z_MIN_PIN));
+
+    Serial.print("Max X Y Z: ")
+    Serial.print(digitalRead(X_MAX_PIN));
+    Serial.print(digitalRead(Y_MAX_PIN));
+    Serial.print(digitalRead(Z_MAX_PIN));
+
     delay(15);
 }
