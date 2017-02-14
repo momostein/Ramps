0450f663258b8a7a2b9f59e63817f919600e8e3c
Momostein - Brecht Ooms <brecht.ooms@hotmail.com>
2017-02-14 22:23:02 +0100

    Completed constructor
    also added temporal values into the measurements.h of the DeltaRobot.ino example

--- examples/DeltaRobot/Measurements.h
+++ examples/DeltaRobot/Measurements.h
@@ -8,6 +8,14 @@
 
 #define STEPS_MM 160 //Steps per mm
 
+//nog aan te passen
+#define TOWER_RADIUS 200
+#define TOWER_HEIGHT 650
+
+#define ARM_LENGHT 300
+
+#define PIVOT_OFFSET 30
+#define TOOL_OFFSET 30
 
 
 #endif
--- src/DeltaRamps.cpp
+++ src/DeltaRamps.cpp
@@ -3,14 +3,33 @@
     Gemaakt door Brecht Ooms
 */
 
-#include "DeltaRamps.h"
 #include "Arduino.h"
+#include "math.h"
+
+#include "DeltaRamps.h"
 
 
-DeltaRamps::DeltaRamps(int _stepsmm):Ramps()
+
+DeltaRamps::DeltaRamps(int _stepsmm, double _towerRadius, double _towerHeight, double _armLenght, double _pivotOffset, double _toolOffset):Ramps()
 {
 	stepsmm = _stepsmm;
 
+	armLenght = _armLenght;
+
+	pivotOffset = _pivotOffset;
+	tooloffset = _tooloffset;
+
+	strutX.x = _towerRadius; // towerRadius * cos(0°)
+	strutY.x = _towerRadius * cos(M_2_PI / 3); // 2/3 * Pi (120°)
+	strutZ.x = _towerRadius * cos(M_2_PI / 3); // 2/3 * Pi (120°)
+
+	strutX.y = 0; // towerRadius * cos(90°)
+	strutY.y = _towerRadius * cos(M_PI_2 / 3); // 1/6 * Pi (30°)
+	strutZ.y = _towerRadius * cos(5 * M_PI_2 / 3); // 5/6 * Pi (150°)
+
+	strutX.z = _towerHeight;
+	strutY.z = _towerHeight;
+	strutZ.z = _towerHeight;
 }
 
 void DeltaRamps::home()
