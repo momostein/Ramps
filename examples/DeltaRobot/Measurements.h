94f8b1f36c95b258458a947e46b6a45de26ebdfb
Unknown <brecht.ooms@hotmail.com>
2017-02-20 13:56:13 +0100

    Put in the right parameters and tested conversion

--- examples/DeltaRobot/Measurements.h
+++ examples/DeltaRobot/Measurements.h
@@ -9,13 +9,13 @@
 #define STEPS_MM 160 //Steps per mm
 
 //nog aan te passen
-#define TOWER_RADIUS 200
-#define TOWER_HEIGHT 650
+#define BASE_SIDE 333.333
+#define TOWER_HEIGHT 622
 
-#define ARM_LENGHT 300
+#define ARM_LENGHT 310
 
-#define PIVOT_OFFSET 30
-#define TOOL_OFFSET 30
+#define PLATFORM_SIDE 84
+#define TOOL_OFFSET 27 //20mm portruding from the platform bottom, 7 mm for the hinge offset
 
 
 #endif
